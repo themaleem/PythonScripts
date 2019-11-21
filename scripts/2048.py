6c1092a56cc1fa2752dd766df751214d7d4bc30b
maleem <zizo9585@gmail.com>
2019-11-21 15:34:37 +0100

    moved .py files into script folder || refactored lyric_finder.py

---
+++ scripts/2048.py
@@ -0,0 +1,50 @@
+_author_ = "Olumide Bello"
+
+"""
+Script dependencies include:
+
+time module
+random module
+selenium module 
+chromedriver
+
+"""
+print("""
+    This is a simple script that plays the popular 2048 game
+    on https://play2048.co imitating a human player such as yourself by sending actual swipes to the game.
+
+    Try it out and see how far you can go before you run out of moves to make! Haha
+""")
+
+from random import choice 
+from selenium import webdriver 
+from time import sleep
+
+#list of selenium keys to be chosen from at random and sent to the browser
+keys = ["Keys.DOWN", "Keys.UP", "Keys.LEFT", "Keys.RIGHT"]
+
+#amount of time the a swipe will be sent to thee browser object
+swipes=int(input('Amount of swipes: '))
+
+#initializing a browser object to be controlled, i used chromedriver
+#you could use FireFox or GeckoDriver, as you wish
+
+try:
+    chrome = webdriver.Chrome()
+
+    #navigation browser object to the 2048 game website
+    chrome.get('https://play2048.co')
+
+    #grabbing the body element of the HTML
+    body = chrome.find_element_by_tag_name('body')
+
+    for i in range(swipes):
+        random_arrow_key=choice(keys)
+    
+        #imitating an actual arrow key (swipe) and sending it to broswer object
+        body.send_keys(random_arrow_key) 
+
+        sleep(1) #wait 1 sec before sending the next command
+except Exception as e:
+    print(e)
+    print('Oops! The error(s) above as shown occured, please correct them and try again.')
---
+++ scripts/lyrics_finder.py
@@ -0,0 +1,69 @@
+_author_  = "olumide bello"
+
+"""
+Script dependencies include:
+
+request module
+BeautifulSoup module
+
+"""
+
+print("""
+    This program scrapes lyrics from the genius lyrics website "https://genius.com"
+    a popular lyrics site and return requested lyrics 
+    All you need is the artiste's name and the song's name
+""")
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_lyrics(artiste,song):
+
+    half_url = "https://genius.com/" #sGenius Lyrics website
+    #this part is needed according to how genius lyrics urls are wired
+    end = "-lyrics" 
+
+    #https://genius.com/Rick-ross-diced-pineapples-lyrics
+    full_url = half_url + artiste + "-" +song + end
+    
+    print(f"attempting to grab your lyrics from {full_url}") 
+
+    try:
+        #request to get data from the url above
+        r = requests.get(full_url)
+
+        #the actual HTML content of the url will be stoted in the variable "data" below 
+        data = r.content
+
+        #beautiful soup object
+        soup = BeautifulSoup(data, "html.parser")
+
+        #finds lyrics text with the html tags which contain the actual lyrics
+        element = soup.find("div", {"class": "lyrics"})
+
+        print("\n")   #newline
+
+        print(element.text.strip()) #prints the final result without preceeding or ending newline characters 
+
+        print("\n")
+    
+    except Exception as e:
+        print(e)
+        print('Oops! error occured while trying to grab your lyrics. Please try again! \nOr try requesting another lyrics')
+
+    finally:
+        print ('Thanks for using the program')
+
+
+    songs_name = input("input the name of the song : ") #eg Diced piNeapplEs
+    
+    # here the above becomes "diced-pineapples"
+    song=songs_name.lower().replace(' ','-')
+    artiste_name = input("input the name of the artiste : ")#Rick Ross
+
+    # becomes rick-ross
+    artiste=artiste_name.lower().replace(' ','-') 
+
+    get_lyrics(artiste,song)
+    
