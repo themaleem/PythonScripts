834c262fde661d35199dfeaaa96a6715af9cde60
maleem <zizo9585@gmail.com>
2019-11-23 21:17:38 +0100

    refactored whatsapp chatter script

--- scripts/wa_chatters.py
+++ scripts/wa_chatters.py
@@ -21,29 +21,33 @@ import re
 
 #Regular Expression pattern to match the beginning of a line of message 
 #please read up RegExp if you can't understand the pattern below
-text=re.compile(b'\d\d/\d\d/\d\d\d\d, \d\d:\d\d - (.*:)') 
+text=re.compile(r'\d\d/\d\d/\d\d\d\d, \d\d:\d\d - (.*:)') 
 
 
 def get_chatters(filepath):
 #     open file in rb(read binary) mode to take care of emoji and byte chat instances
+        #Milestone 2
     with open(filepath,'rb') as file:
         #   initialize a variable (file_content) for byte-like string with b''
         #   and add each line in the file to vaiable 
-        file_content=b''    
+        file_content=''    
         for i in file:
-            file_content+=i
+        #   decode() methods changes the byte-stream into a str object
+            file_content+=i.decode('utf-8')
     
     chatters=[]
     #find all matches of 'text' pattern in 'file_content' 
     patterns=re.findall(text,file_content)
 
+    #Milestone 3.1
+    print(f'This chat contains {len(patterns)} sent messages')
+    
     for i in patterns:
-    #   decode() methods changes the byte-stream into a str object
     #   the regexp finds all matches even after the first semi-colon ':' but we really only need the first :
     #   so we split whatever it returns at every semicolon ':' 
     #   and grab the first item of the splitted list
     #   thats what we really need, the sender of message in the chat
-        i=i.decode('utf-8').split(':')[0]
+        i=i.split(':')[0]
         
         #   append sender's name if not already in chatter's list
         if i not in chatters: 
